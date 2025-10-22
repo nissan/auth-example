88c979f69ab6dbfdebe7b77790482955711f8ef8
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-10-22 11:43:04 +1000

    adds minimal register model

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel, EmailStr, Field, constr, validator
 from datetime import datetime
 
 app = FastAPI(title="Simple Backend ")
+
+class UserCreateModel(BaseModel):
+    name: str
+    email: EmailStr
+    date_of_birth: datetime
+    job_title: str
+    password: str          
 
 class UserViewModel(BaseModel):
     id: str
@@ -15,6 +22,17 @@ class UserViewModel(BaseModel):
 
 @app.get("/users/{user_id}", response_model=UserViewModel)
 def get_user(user_id: str) -> UserViewModel:  
+    dummy_user = UserViewModel(
+        id="1",
+        name="John Smith",
+        email="john@acme.com",
+        date_of_birth=datetime.fromisoformat("1980-04-14T10:00:00+00:00"), 
+        job_title="Janitor",
+    )
+    return dummy_user
+
+@app.post("/users",response_model=UserViewModel,status_code=status.HTTP_201_CREATED)
+def create_user(payload: UserCreateModel) -> UserViewModel:
     dummy_user = UserViewModel(
         id="1",
         name="John Smith",
