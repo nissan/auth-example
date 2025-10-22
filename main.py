52cca6dd18b3f8e8f2788e8d1eba82c86d2cd055
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-10-22 10:31:05 +1000

    add simple get user, add some http tests

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr, Field, constr, validator
+from datetime import datetime
 
-app = FastAPI()
+app = FastAPI(title="Simple Backend ")
+
+class UserViewModel(BaseModel):
+    id: str
+    name: str
+    email: EmailStr
+    date_of_birth: datetime
+    job_title: str
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/users/{user_id}", response_model=UserViewModel)
+def get_user(user_id: str) -> UserViewModel:  
+    dummy_user = UserViewModel(
+        id="1",
+        name="John Smith",
+        email="john@acme.com",
+        date_of_birth=datetime.fromisoformat("1980-04-14T10:00:00+00:00"), 
+        job_title="Janitor",
+    )
+    return dummy_user
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
